9e80e783490581bff4ad3759ec68173d0640b9ba
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-20 13:45:58 -0700

    Bubble sort

--- sort.h
+++ sort.h
@@ -7,6 +7,6 @@
 /* Protoytpes */
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/*void print_list(const listint_t *list);*/
 
 #endif

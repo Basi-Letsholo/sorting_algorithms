690615d33378489c20f5e8bd35f9922508839448
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-07-25 06:07:37 -0700

    betty fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,53 +1,75 @@
 #include "sort.h"
 
+/**
+ * partition - sorts partition of list
+ * @array: array of list
+ * @size: length of list
+ * @left: starting index
+ * @right: ending index
+ * Return: pivot index
+ */
 int partition(int *array, size_t size, int left, int right)
 {
-    int *temp, i, j;
-    int tmp;
+	int *temp, i, j;
+	int tmp;
 
-    temp = array + right;
-    for (i = j = left; j < right; j++)
-    {
-        if (array[j] < *temp)
-        {
-            if (i < j)
-            {
-                tmp = array[i];
-                array[i] = array[j];
-                array[j] = tmp;
-                print_array(array, size);
-            }
-            i++;
-        }
-    }
+	temp = array + right;
+	for (i = j = left; j < right; j++)
+	{
+		if (array[j] < *temp)
+		{
+			if (i < j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		i++;
+		}
+	}
 
-    if (array[i] > *temp)
-    {
-        tmp = array[i];
-        array[i] = array[right];
-        array[right] = tmp;
-        print_array(array, size);
-    }
+	if (array[i] > *temp)
+	{
+		tmp = array[i];
+		array[i] = array[right];
+		array[right] = tmp;
+		print_array(array, size);
+	}
 
-    return (i);
+	return (i);
 }
 
+/**
+ * lomuto_sort - implements lomuto partition
+ * @array: array of list
+ * @size: length of list
+ * @left: starting index
+ * @right: ending index
+ */
 void lomuto_sort(int *array, size_t size, int left, int right)
 {
-    int pivot;
+	int pivot;
 
-    if (right - left > 0)
-    {
-        pivot = partition(array, size, left, right);
-        lomuto_sort(array, size, left, pivot - 1);
-        lomuto_sort(array, size, pivot + 1, right);
-    }
+	if (right - left > 0)
+	{
+		pivot = partition(array, size, left, right);
+		lomuto_sort(array, size, left, pivot - 1);
+		lomuto_sort(array, size, pivot + 1, right);
+	}
 }
 
+/**
+ * quick_sort - sorts list using quick sort algorithm
+ * @array: array of list
+ * @size: length of list
+ */
 void quick_sort(int *array, size_t size)
 {
-    if (size < 2)
-    return;
+	if (size < 2)
+	{
+		return;
+	}
 
-    lomuto_sort(array, size, 0, size - 1);
+	lomuto_sort(array, size, 0, size - 1);
 }
